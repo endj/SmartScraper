5a76efbe294ced951cca2b9300c83b5f188ed323
endj <edin.j.1992@gmail.com>
2017-01-29 17:47:16 +0100

    Added the resultpage which lists the input of the main activity

--- app/src/main/java/se/edinjakupovic/mobilescraper/MainActivity.java
+++ app/src/main/java/se/edinjakupovic/mobilescraper/MainActivity.java
@@ -1,13 +1,27 @@
 package se.edinjakupovic.mobilescraper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MESSAGE = "N";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void doSearch(View view){
+        Intent intent = new Intent(this, ResultPage.class);  // An intent is used to do something
+        EditText input = (EditText) findViewById(R.id.input); // Fiends the search bar
+        String intext = input.getText().toString(); // Gets the search message
+        intent.putExtra(MESSAGE,intext);  // Adds
+        startActivity(intent);
+    }
+    
 }
