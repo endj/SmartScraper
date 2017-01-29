c48833890a3ad386ea61752529e2c769d56f9ffb
jnsjns <spl4sh@live.se>
2017-01-29 23:22:07 +0100

    testing .gitignore

--- app/src/main/java/se/edinjakupovic/mobilescraper/MainActivity.java
+++ app/src/main/java/se/edinjakupovic/mobilescraper/MainActivity.java
@@ -1,5 +1,5 @@
 package se.edinjakupovic.mobilescraper;
-//what up edin ok ändring nummer 23
+//testing git
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
