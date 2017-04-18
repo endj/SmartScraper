5c4e3998a460260d9c652da94d452e2ae1196fd5
endj <edin.j.1992@gmail.com>
2017-04-18 17:03:13 +0200

    Update functions.php
    Added functioon for retrieving top relevancy scores

--- functions.php
+++ functions.php
@@ -1,6 +1,21 @@
 <?php
 include 'db.php';
 
+
+function return_UrlRelevanceMapping($conn){
+    $stmt = $conn->prepare("SELECT url,relevance
+                            FROM search
+                            WHERE s_id = (SELECT searchterm.id FROM searchterm WHERE
+                                          term = :term) ORDER BY 'relevance' LIMIT 10");
+    $stmt->execute([':term' => $_POST['search']]);
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+      echo $row['url']." ".$row['relevance']." ";
+
+    }
+
+
+}
+
 function new_search($conn,$links){
 
   $stmt = $conn->prepare("INSERT INTO searchterm(term,noOfSearches) VALUES (:term, 1)
@@ -19,14 +34,12 @@ function new_search($conn,$links){
           WHERE domain.id = (SELECT domain.id FROM domain WHERE url = :url) AND
           searchterm.id = (SELECT searchterm.id FROM searchterm where term = :term)");
 
-        $stmt->bindParam(':term',$_POST['search']);
+        $stmt->bindParam(':term',$_POST['search']); // stops sql injection
           for($x=0; $x < $links; $x++){
             $stmt->bindParam(':search', $_POST['searchUrl'.$x]);
             $stmt->bindParam(':url', $_POST['domainUrl'.$x]);
             $stmt->execute();
           }
-
-
 }
 
  ?>
