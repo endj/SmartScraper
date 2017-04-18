a7c542022055f204318206d9f9c418d5242b7f26
endj <edin.j.1992@gmail.com>
2017-04-18 17:04:17 +0200

    Update script.php
    Added function for getting relevant links

--- script.php
+++ script.php
@@ -2,21 +2,13 @@
 include 'db.php';
 include 'functions.php';
 
-  if(isset($_POST['search'])){ // Http request
-    //echo $_POST['domainUrl0'];
+  if(isset($_POST['search'])){
+
     $links = $_POST['numOfLinks'];
     new_search($conn,$links);
 
+    return_UrlRelevanceMapping($conn); // Return most relevant searches for searchterm
 
-    $x = 0;
-    foreach ($_POST as $key => $value) {
-    //  echo $x;
-      $x++;
-    }
-
-
-    //new_search($conn);
-  //  new_domain($conn);
   }
 
 
