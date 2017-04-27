61e5bb5a92c99d0c48dd552504a56953c68fc6d9
endj <edin.j.1992@gmail.com>
2017-04-27 21:26:19 +0200

    Update functions.php
    Added relevance update

--- functions.php
+++ functions.php
@@ -34,12 +34,46 @@ function new_search($conn,$links){
           WHERE domain.id = (SELECT domain.id FROM domain WHERE url = :url) AND
           searchterm.id = (SELECT searchterm.id FROM searchterm where term = :term)");
 
-        $stmt->bindParam(':term',$_POST['search']); // stops sql injection
+        $stmt->bindParam(':term',$_POST['search']); // stops
           for($x=0; $x < $links; $x++){
             $stmt->bindParam(':search', $_POST['searchUrl'.$x]);
             $stmt->bindParam(':url', $_POST['domainUrl'.$x]);
             $stmt->execute();
           }
+}
+
+
+function test($conn){
+  $stmt = $conn->prepare("UPDATE search SET relevance = 1
+                          WHERE d_id = '3637'");
+  $stmt->execute();
+}
+
+function updateRelevancy($conn,$upvotes,$downvotes){
+
+  $stmt = $conn->prepare("UPDATE search SET relevance = relevance+0.01
+                        WHERE s_id = (SELECT searchterm.id FROM searchterm WHERE term = :s_id)
+                        AND d_id = (SELECT domain.id FROM domain WHERE url = :d_id)
+                        AND url = :url");
+            for($x=0; $x < $upvotes; $x++){
+              $stmt->bindParam(':url',$_POST['upvoteURL'.$x]);
+              $stmt->bindParam(':d_id',$_POST['upvoteDomain'.$x]);
+              $stmt->bindParam(':s_id',$_POST['searchTerm']);
+              $stmt->execute();
+            }
+
+  $stmt = $conn->prepare("UPDATE search SET relevance = relevance-0.01
+                        WHERE s_id = (SELECT searchterm.id FROM searchterm WHERE term = :s_id)
+                        AND d_id = (SELECT domain.id FROM domain WHERE url = :d_id)
+                        AND url = :url");
+            for($x=0; $x < $downvotes; $x++){
+              $stmt->bindParam(':url',$_POST['downvoteURL'.$x]);
+              $stmt->bindParam(':d_id',$_POST['downvoteDomain'.$x]);
+              $stmt->bindParam(':s_id',$_POST['searchTerm']);
+              $stmt->execute();
+            }
+
+
 }
 
  ?>
